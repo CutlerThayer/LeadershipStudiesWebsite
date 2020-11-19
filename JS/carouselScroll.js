e44012d0295709faffd69f9c0e8835c3454234b8
Cutler <cutlerthayer@gmail.com>
2020-11-19 15:11:02 -0600

    added the navbar to the index

--- JS/carouselScroll.js
+++ JS/carouselScroll.js
@@ -1,3 +1,25 @@
+var slideIndex = 0;
+carousel();
+
 function carousel(){
 
+    var i;
+
+    var slides = index.html.getElementsByClassName("Image-Slider");
+
+    var dots = index.html.getElementsByClassName("Dots");
+
+    for(i = 0; i < slides.length; ++i){
+        slides[i].style.display = "none";
+    }
+
+    ++slideIndex;
+
+    if(slideIndex > slides.length){
+        slideIndex = 1;
+    }
+
+    for(i = 0 ; i < dots.length; ++i){
+        
+    }
 }
